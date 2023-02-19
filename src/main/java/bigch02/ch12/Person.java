0688b0a90214968f94087f44ed2cd5aa7bb98513
임성준 <zkfmak10@gmail.com>
2023-02-19 13:13:03 +0900

    ch12

--- src/main/java/bigch02/ch12/Person.java
+++ src/main/java/bigch02/ch12/Person.java
@@ -1,4 +1,25 @@
 package bigch02.ch12;
 
 public class Person {
+    String name;
+    int age;
+
+    public Person() {
+        this("no name", 1);
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person getPerson() {
+        return this;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        Person person2 = person.getPerson();
+        System.out.println(person2);
+    }
 }
